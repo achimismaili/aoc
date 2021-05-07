81bf4cf6d1d9d48ea90d6dee72a8ee0d728f9dd9
Achim Ismaili <achim.ismaili@telekom.de>
2021-05-07 16:06:21 +0200

    added some variables to the hello.py file

--- day01/py/hello.py
+++ day01/py/hello.py
@@ -1,4 +1,10 @@
 # sample hello world test file
 # see also https://docs.microsoft.com/de-de/azure/machine-learning/tutorial-1st-experiment-hello-world
 
-print("Hello world!")
+year = 2021
+
+name = "Peter Pan"
+
+message = "Hello world!"
+
+print(message)
