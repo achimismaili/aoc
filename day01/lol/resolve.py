d0191fd0450fc7bfd30e80294d21fcff0d4b0623
Leon-creator <leon@ismaili.de>
2021-05-07 18:08:19 +0200

    Read file succesfully

---
+++ day01/lol/resolve.py
@@ -0,0 +1,14 @@
+# arrange
+
+with open(".\day01\lol\sample.txt", "r") as file:
+    for line in file:
+        print (line)
+# act
+
+# assert
+
+
+# i = 1
+# while i <= 6:
+#     print(i)
+#     i += 1
